171d07c3e276de7cc9ffef60a950787df36bf571
Filipe Esperandio <filipesperandio@gmail.com>
2017-09-08 14:11:19 -0300

    Sanity check test

---
+++ test/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public boolean someLibraryMethod() {}
+
+	public void foo() {
+		for (int i = 0; i < 10; i++) {
+			for (int k = 0; k < 20; k++) {
+				System.out.println("Hello");
+			}
+		}
+
+		for (int i = 0; i < 10; i++) {
+			for (int k = 0; k < 20; i++) {
+				System.out.println("Hello");
+			}
+		}
+	}
+}
