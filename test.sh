f87dacab1c0f2ced0367bc9dd25f49a4f2b7943d
Filipe Esperandio <filipesperandio@gmail.com>
2017-09-21 12:53:44 -0300

    Adjust test script

--- test.sh
+++ test.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env sh
+set -e
 
+BUILD_DIR=build
 GROOVY="${GROOVY_HOME}/embeddable/groovy-all-${GROOVY_VERSION}.jar"
 JUNIT=$(ls ${GROOVY_HOME}/lib/junit*.jar)
 HAMCREST=$(ls ${GROOVY_HOME}/lib/hamcrest*.jar)
 
-TEST_CLASSES=$(find test -name "*.groovy" | sed -E 's#test/(.*).groovy#\1#' | xargs)
+test_classes() {
+  find test -name "*.groovy" | sed -E 's#test/(.*).groovy#\1#' | xargs
+}
 
-rm -rf build
+clean() {
+  rm -rf $BUILD_DIR
+}
 
-groovyc test/**.groovy -d build
+build() {
+  groovyc test/**.groovy -d $BUILD_DIR
+}
 
+run() {
+  java -cp build:$JUNIT:$HAMCREST:$GROOVY org.junit.runner.JUnitCore $(test_classes)
+}
 
-java -cp build:$JUNIT:$HAMCREST:$GROOVY org.junit.runner.JUnitCore $TEST_CLASSES
+clean
+build
+run
