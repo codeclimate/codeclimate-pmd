7c70ff62e188250fada826325ccddf4a04178b4b
Filipe Esperandio <filipesperandio@gmail.com>
2017-09-25 17:41:35 -0300

    Expectations come first

--- test/ConfigTest.groovy
+++ test/ConfigTest.groovy
@@ -5,19 +5,19 @@ class ConfigTest {
   @Test
   public void defaultRuleSet() {
     def config = new Config([configFile: "/usr/src/app/fixtures/default/config.json", codeFolder: "/usr/src/app/fixtures/default"])
-    assertEquals config.ruleSet(), "/usr/src/app/ruleset.xml"
+    assertEquals "/usr/src/app/ruleset.xml", config.ruleSet()
   }
 
   @Test
   public void specifiedRuleSetFile() {
     def config = new Config([configFile: "/usr/src/app/fixtures/specified_file/config.new.json", codeFolder: "/usr/src/app/fixtures/specified_file"])
-    assertEquals config.ruleSet(), "/usr/src/app/fixtures/specified_file/rules.xml"
+    assertEquals "/usr/src/app/fixtures/specified_file/rules.xml", config.ruleSet()
   }
 
   @Test
   public void honorPresentRules() {
     def config = new Config([configFile: "/usr/src/app/fixtures/ruleset_default_file/config.json", codeFolder: "/usr/src/app/fixtures/ruleset_default_file"])
-    assertEquals config.ruleSet(), "/usr/src/app/fixtures/ruleset_default_file/ruleset.xml"
+    assertEquals "/usr/src/app/fixtures/ruleset_default_file/ruleset.xml", config.ruleSet()
   }
 
   @Test(expected = IllegalArgumentException.class)
@@ -29,6 +29,6 @@ class ConfigTest {
   @Test
   public void acceptRulesSimpleNames() {
     def config = new Config([configFile: "/usr/src/app/fixtures/rules/config.json", codeFolder: "/usr/src/app/fixtures/rules"])
-    assertEquals config.ruleSet(), "java-basic,java-design"
+    assertEquals "java-basic,java-design", config.ruleSet()
   }
 }
