a4278076cc9920c54aa1dcfa7f43d043f969da01
Filipe Esperandio <filipesperandio@gmail.com>
2017-09-25 18:04:27 -0300

    Allow rules string as a configuration alternative (#19)

--- test/ConfigTest.groovy
+++ test/ConfigTest.groovy
@@ -5,18 +5,30 @@ class ConfigTest {
   @Test
   public void defaultRuleSet() {
     def config = new Config([configFile: "/usr/src/app/fixtures/default/config.json", codeFolder: "/usr/src/app/fixtures/default"])
-    assertEquals config.ruleSet(), "/usr/src/app/ruleset.xml"
+    assertEquals "/usr/src/app/ruleset.xml", config.ruleSet()
   }
 
   @Test
   public void specifiedRuleSetFile() {
     def config = new Config([configFile: "/usr/src/app/fixtures/specified_file/config.new.json", codeFolder: "/usr/src/app/fixtures/specified_file"])
-    assertEquals config.ruleSet(), "/usr/src/app/fixtures/specified_file/rules.xml"
+    assertEquals "/usr/src/app/fixtures/specified_file/rules.xml", config.ruleSet()
   }
 
   @Test
   public void honorPresentRules() {
     def config = new Config([configFile: "/usr/src/app/fixtures/ruleset_default_file/config.json", codeFolder: "/usr/src/app/fixtures/ruleset_default_file"])
-    assertEquals config.ruleSet(), "/usr/src/app/fixtures/ruleset_default_file/ruleset.xml"
+    assertEquals "/usr/src/app/fixtures/ruleset_default_file/ruleset.xml", config.ruleSet()
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void doesNotAllowToMixRulesAndFile() {
+    def config = new Config([configFile: "/usr/src/app/fixtures/rules/config.mix.json", codeFolder: "/usr/src/app/fixtures/rules"])
+    config.ruleSet()
+  }
+
+  @Test
+  public void acceptRulesSimpleNames() {
+    def config = new Config([configFile: "/usr/src/app/fixtures/rules/config.json", codeFolder: "/usr/src/app/fixtures/rules"])
+    assertEquals "java-basic,java-design", config.ruleSet()
   }
 }
